163e21158ac57cd91a201b99e96fab1d29818cd3
dchecheb <qocodbssla@naver.com>
2021-05-18 21:02:42 +0900

    feat: getMain, getPosts, postPosts 컨트롤러 추가

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,9 +1,71 @@
 export const getMain = async (req, res, next) => {
-  return res.send("getMain");
+  try {
+    // db query
+    const sumPostNum = await Post.find({}).countDocuments();
+    const todayPostNum = await Post.find({
+        createAt: {
+            $gte: new Date(new Date().setHours(0, 0, 0, 0)),
+            $lte: new Date(new Date().setHours(23,59,59,999))
+        }
+    }).countDocuments();
+    const sumUserNum = await Post.find({
+        createAt: {
+            $gte: new Date(new Date().setHours(0, 0, 0, 0)),
+            $lte: new Date(new Date().setHours(23,59,59,999))
+        },
+        distinct: writer
+    }).countDocuments();
+
+    // 응답
+    return res.status(200).json({
+        sumPostNum,
+        todayPostNum,
+        sumUserNum
+    });
+} catch(error) {
+    next(error);
+}
 };
+
 export const getPosts = async (req, res, next) => {
-  return res.send("getPosts");
+  try {
+    const {query : {category, sort}} = req;
+    if (!category || !sort) {
+        return res.status(400).send({err: 'category or sort is not exist.'});
+    }
+    // 카테고리 별
+    let filters = {};
+    if (category !== 'all') {
+        filters['category'] == category;
+    };
+
+    // 정렬
+    if (sort === 'likes') {
+        let posts = await Post.find(filter)
+        .sort({likeCount: 1});
+    } else {
+        let posts = await Post.find(filter)
+        .sort({createAt: -1});
+    };
+
+    // 응답
+    return res.status(200).json({ posts });
+  } catch(error) {
+    next(error);
+  }
 };
+
 export const postPost = async (req, res, next) => {
-  return res.send("postPost");
+  try {
+    let variable = req.body;
+    variable.writer = req.user;
+
+    // db query
+    await Post.create(variable);
+
+    // 응답
+    return res.status(200);
+  } catch(error) {
+    next(error);
+  }
 };
