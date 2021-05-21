cb22dbfce5232e9ae6c0aff1a528fb66ee43c05a
dchecheb <qocodbssla@naver.com>
2021-05-22 05:00:05 +0900

    feat: 좋아요 취소 api 추가

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const postLike = async (req, res, next) => {
     try {
         const {
-            query: { postId },
+            body: { postId },
         } = req;
         if (!mongoose.isValidObjectId(postId)) {
             return res.status(400).send({ err: "Invalid Post ID" });
@@ -16,7 +16,29 @@ export const postLike = async (req, res, next) => {
         );
         await User.updateOne(
                 {_id: req.user._id},
-                {$addToSet: {likePost: postId}}
+                {$push: {likePost: postId}}
+            );
+        return res.status(200).json({success: true});
+    } catch(error) {
+        next(error);
+    }
+}
+
+export const deleteLike = async (req, res, next) => {
+    try {
+        const {
+            query: { postId },
+        } = req;
+        if (!mongoose.isValidObjectId(postId)) {
+            return res.status(400).send({ err: "Invalid Post ID" });
+        }
+        await Post.updateOne(
+            {_id: postId},
+            {$inc: {likeNum: -1}}
+        );
+        await User.updateOne(
+                {_id: req.user._id},
+                {$pull: {likePost: postId}}
             );
         return res.status(200).json({success: true});
     } catch(error) {
